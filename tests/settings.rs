98400dd137e3e00d48e5360c40a221574db45792
Maxim Zhiburt <zhiburt@gmail.com>
2021-06-30 15:33:25 +0300

    Add tests for settings
    set_echo currently fails
    
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

---
+++ tests/settings.rs
@@ -0,0 +1,38 @@
+use ptyprocess::PtyProcess;
+use std::{process::Command, time::Duration};
+
+#[test]
+fn default_win_size() {
+    let proc = PtyProcess::spawn(Command::new("cat")).unwrap();
+
+    assert_eq!(proc.get_window_size().unwrap(), (80, 24));
+}
+
+#[test]
+fn set_win_size() {
+    let mut proc = PtyProcess::spawn(Command::new("cat")).unwrap();
+
+    proc.set_window_size(100, 200).unwrap();
+
+    assert_eq!(proc.get_window_size().unwrap(), (100, 200));
+}
+
+#[test]
+fn default_echo() {
+    let proc = PtyProcess::spawn(Command::new("cat")).unwrap();
+
+    assert_eq!(proc.get_echo().unwrap(), false);
+}
+
+#[test]
+fn set_echo() {
+    let mut proc = PtyProcess::spawn(Command::new("cat")).unwrap();
+
+    assert!(proc.isatty().unwrap());
+
+    proc.set_echo(true).unwrap();
+    proc.wait_echo(true, Some(Duration::from_millis(500)))
+        .unwrap();
+
+    assert_eq!(proc.get_echo().unwrap(), true);
+}
