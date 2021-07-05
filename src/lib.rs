125b2e0748e524297856f12dec23c4cd52ce6c4d
Maxim Zhiburt <zhiburt@gmail.com>
2021-07-05 20:29:13 +0300

    Add a notion about Async in doc comment of lib.rs
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,31 @@
 //!
 //! let mut process = PtyProcess::spawn(Command::new("cat")).expect("failed to spawn a process");
 //!
-//! process.write_all(b"hello cat\n").expect("failed write");
+//! process.write_all(b"hello cat\n").expect("failed to write");
 //! let mut buf = vec![0; 128];
-//! let size = process.read(&mut buf).expect("failed read");
+//! let size = process.read(&mut buf).expect("failed to read");
 //! assert_eq!(&buf[..size], b"hello cat\r\n");
 //!
 //! let sucess = process.exit(true).expect("failed to exit");
 //!
 //! assert_eq!(sucess, true);
+//! ```
+//!
+//! # Async
+//! 
+//! The library provides an async IO operations for communication.
+//! To be able to use them you must to provide a feature flag `[async]`
+//! and turn off default features `default-features = false`
+//! 
+//! ## Example
+//! 
+//! ```no_run,ignore
+//! use ptyprocess::PtyProcess;
+//! use std::process::Command;
+//!
+//! let mut process = PtyProcess::spawn(Command::new("cat")).expect("failed to spawn a process");
+//!
+//! process.send_line("hello cat").await.expect("failed writing");
 //! ```
 
 mod control_code;
