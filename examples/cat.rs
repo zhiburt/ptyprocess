c8989ccc0bdec1db6950ebf8ff94fe1baf02c7cf
Maxim Zhiburt <zhiburt@gmail.com>
2021-12-04 22:11:57 +0300

    Fix cat.rs
    To not use read_to_end

--- examples/cat.rs
+++ examples/cat.rs
@@ -8,8 +8,28 @@ use std::{
 };
 
 fn main() {
-    let mut process = PtyProcess::spawn(Command::new("echo").arg("hello world")).expect("Error while spawning process");
-    println!("w8");
-process.wait();
-println!("done");
+    let process = PtyProcess::spawn(Command::new("cat")).expect("Error while spawning process");
+    let mut stream = process
+        .get_pty_stream()
+        .expect("Failed to get a pty handle");
+
+    let mut this_file = File::open(".gitignore").expect("Can't open a file");
+    io::copy(&mut this_file, &mut stream).expect("Can't copy a file");
+
+    // EOT
+    stream
+        .write_all(&[4])
+        .expect("Error while exiting a process");
+
+    // We can't read_to_end as the process isn't DEAD but at time time it is it's already a EOF
+
+    let mut buf = [0; 128];
+    loop {
+        let n = stream.read(&mut buf).expect("Erorr on read");
+        print!("{}", String::from_utf8_lossy(&buf[..n]));
+
+        if n == 0 {
+            break;
+        }
+    }
 }
