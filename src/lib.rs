3be5c8da4c2b4627a8bd38fff1da20bc1edea2d1
Maxim Zhiburt <zhiburt@gmail.com>
2021-12-05 18:32:49 +0300

    Use O_NOCTTY on all platforms
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -453,7 +453,7 @@ impl Master {
         let slave_name = self.get_slave_name()?;
         let slave_fd = open(
             format!("/dev/{}", slave_name.as_str()).as_str(),
-            OFlag::O_RDWR,
+            OFlag::O_RDWR | OFlag::O_NOCTTY,
             Mode::empty(),
         )?;
         Ok(slave_fd)
